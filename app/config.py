6913d58d31e02bdbf189ffbb24dd28193b9d63d3
Matthew Palmer <palmer.matthew4567@gmail.com>
2021-03-28 02:00:12 -0500

    Starter Flask App

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    """Base Config Object"""
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # UPLOAD_FOLDER = './uploads'
+
+
+class DevelopmentConfig(Config):
+    """Development Config that extends the Base Config Object"""
+    DEVELOPMENT = True
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Production Config that extends the Base Config Object"""
+    DEBUG = False
---
+++ app/views.py
@@ -0,0 +1,35 @@
+from app import app
+from flask import render_template, url_for, redirect, flash, request, session
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page. If we wanted
+    to we could change max-age to 600 seconds which would be 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
+"""Yannik Lyn Fatt"""
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+    
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
